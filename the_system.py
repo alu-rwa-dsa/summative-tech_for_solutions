fa5d559d0212037b851f9f44e4187b8b61362bb9
SylvieMutuyimana <90443417+SylvieMutuyimana@users.noreply.github.com>
2022-04-11 22:38:30 +0300

    Add files via upload

---
+++ the_system.py
@@ -0,0 +1,14 @@
+# import openpyxl and tkinter modules
+# importing Mapping to avoid any errors
+from collections.abc import Mapping
+from openpyxl import *
+from tkinter import *
+import queue
+import random
+
+# create the document variable
+applications_document = load_workbook('C:NSO_assets\\NSO_applications.xlsx')
+members_document = load_workbook('C:NSO_assets\\NSO_members.xlsx')
+#an excel function to handle the excel document
+applications = []
+members = []
